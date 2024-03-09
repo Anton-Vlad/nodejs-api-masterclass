9dbe6d4e367bfdfdcd96e39d5d5c55c0bbc97db7
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-03-09 22:13:28 +0200

    Custom ErrorHandlers

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 // const logger = require("./middleware/logger");
 const morgan = require("morgan");
 const colors = require("colors");
+const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
 // Load env vars
@@ -17,27 +18,30 @@ const app = express();
 app.use(express.json());
 
 // Dev logging middleware
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
-
-// Mount routers
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
 
+// Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port`.yellow.bold);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port`.yellow.bold
+  );
 });
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
+process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`.red);
 
   // Close server and exit process
   server.close(() => process.exit(1));
-})
+});
