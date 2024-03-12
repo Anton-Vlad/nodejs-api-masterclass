9a906baa7821c2c6ca861503d6ca90fa9ec412e4
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-03-12 23:23:03 +0200

    First advanced mongo query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require("./../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
+const geocoder = require("../utils/geocoder");
 const Bootcamp = require("./../models/Bootcamp");
 
 // @desc    Get all bootcamps
@@ -73,4 +74,33 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   }
 
   return res.status(200).json({ success: true, data: {} });
+});
+
+// @desc    Get bootcamps within a Radius
+// @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Cal radius using radians
+  // Divide distance by radius of Earth
+  // Earth radius = 3,963 mi  / 6,378 km
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: {
+      $geoWithin: { $centerSphere: [[lng, lat], radius] },
+    },
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
 });
