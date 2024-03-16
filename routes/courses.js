40cbd1d9538a611727a13c3b414372d1d14aaaf4
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-03-16 17:15:17 +0200

    UserAuth - Protect routes & Store token in ThinderClient env

--- routes/courses.js
+++ routes/courses.js
@@ -10,6 +10,7 @@ const router = express.Router({ mergeParams: true });
 
 const Course = require("../models/Course");
 const advancedResults = require("../middleware/advancedResults");
+const { protect } = require("../middleware/auth");
 
 router
   .route("/")
@@ -20,7 +21,11 @@ router
     }),
     getCourses
   )
-  .post(addCourse);
-router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse);
+  .post(protect, addCourse);
+router
+  .route("/:id")
+  .get(getCourse)
+  .put(protect, updateCourse)
+  .delete(protect, deleteCourse);
 
 module.exports = router;
