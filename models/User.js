a220215d7493857a64010766fada0c6b552f963f
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-03-18 22:49:49 +0200

    ForgotPassword - Generate new token

--- models/User.js
+++ models/User.js
@@ -38,14 +38,15 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt password using bcrypt
 UserSchema.pre("save", async function (next) {
-  // if (!this.isModified('password')) {
-  //   next();
-  // }
+  if (!this.isModified("password")) {
+    next();
+  }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// !* Methods are called on the instance of a user (lowerCase) *!
 // Sign JWT and return
 UserSchema.methods.getSignedJwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
@@ -58,21 +59,23 @@ UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-// // Generate and hash password token
-// UserSchema.methods.getResetPasswordToken = function() {
-//   // Generate token
-//   const resetToken = crypto.randomBytes(20).toString('hex');
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString("hex");
 
-//   // Hash token and set to resetPasswordToken field
-//   this.resetPasswordToken = crypto
-//     .createHash('sha256')
-//     .update(resetToken)
-//     .digest('hex');
+  console.log(resetToken);
 
-//   // Set expire
-//   this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
 
-//   return resetToken;
-// };
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000; // Expires in 10 minutes
+
+  return resetToken;
+};
 
 module.exports = mongoose.model("User", UserSchema);
