3fc9277dc2e81ccd82171871c2009bf0ef707c57
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-03-20 21:50:55 +0200

    Reviews - 60%

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getReviews } = require("../controllers/reviews");
+const { getReviews, getReview } = require("../controllers/reviews");
 const router = express.Router({ mergeParams: true });
 
 const Review = require("../models/Review");
@@ -13,5 +13,6 @@ router.route("/").get(
   }),
   getReviews
 );
+router.route("/:id").get(getReview);
 
 module.exports = router;
--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/config.env" });
 // Load models
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const Review = require("./models/Review");
 const User = require("./models/User");
 
 // Connect to db
@@ -24,6 +25,9 @@ const courses = JSON.parse(
 const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
 );
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, "utf-8")
+);
 
 // Import into DB
 const importData = async () => {
@@ -31,6 +35,7 @@ const importData = async () => {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
 
     console.log("Data Imported...".green.inverse);
     process.exit();
@@ -45,6 +50,7 @@ const deleteData = async () => {
     await Bootcamp.deleteMany(); // delete all records
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
 
     console.log("Data Destroyed...".red.inverse);
     process.exit();
