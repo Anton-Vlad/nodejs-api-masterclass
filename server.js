ffa272f2953d76839f2054fa75efe31467bf98a3
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-03-20 21:33:56 +0200

    Reviews - 30%

--- server.js
+++ server.js
@@ -37,12 +37,14 @@ app.use(express.static(path.join(__dirname, "public")));
 // Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const reviews = require("./routes/reviews");
 const auth = require("./routes/auth");
 const users = require("./routes/users");
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/reviews", reviews);
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/users", users);
 
