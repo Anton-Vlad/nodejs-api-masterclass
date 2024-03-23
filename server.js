9036f6e58713804b3ecae6289b4f8214ebe54e76
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2024-03-23 11:39:44 +0200

    Security - Prevent NoSQL injections

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 // const logger = require("./middleware/logger");
 const morgan = require("morgan");
+const mongoSanitize = require('express-mongo-sanitize');
 const path = require("path");
 const colors = require("colors");
 const fileUpload = require("express-fileupload");
@@ -30,6 +31,9 @@ if (process.env.NODE_ENV === "development") {
 
 //File uploading
 app.use(fileUpload());
+
+// Sanitize data
+app.use(mongoSanitize());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
